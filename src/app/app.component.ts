b8641f0a32bb0fbd26c2d42371e5c549287f699c
phi <phungvietphi88@gmail.com>
2023-05-09 15:56:12 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+  myName: string = "Phung Viet Phi";
+  myAge: number = 20;
+  myStatus: boolean = true;
+  myInfo: { name: string, age: number } = {
+    name: "Phung Viet Phi",
+    age: 20
+  };
+
+  showInfo(name: string) {
+    return `My name is ${name ? name : this.myName} and I'm ${this.myAge} years old`;
+  }
 }
